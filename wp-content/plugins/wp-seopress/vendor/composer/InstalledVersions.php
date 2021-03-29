77f5064f0a7d24e6aedc95abbb6c2ab1cde386f3
Das Neue Fuehren <posfuehr312521@corporate-happiness.de>
2021-03-29 17:02:48 +0200

    Plugins Update

--- wp-content/plugins/wp-seopress/vendor/composer/InstalledVersions.php
+++ wp-content/plugins/wp-seopress/vendor/composer/InstalledVersions.php
@@ -24,24 +24,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '4.4.x-dev',
-    'version' => '4.4.9999999.9999999-dev',
+    'pretty_version' => '4.5.x-dev',
+    'version' => '4.5.9999999.9999999-dev',
     'aliases' => 
     array (
     ),
-    'reference' => '04b62bb8945e4b2983cfc8d1230addc555371136',
+    'reference' => 'c5a0e845031b44c1d5f1a3bb29e943c2d836d2b9',
     'name' => 'wp-seopress/wp-seopress',
   ),
   'versions' => 
   array (
     'wp-seopress/wp-seopress' => 
     array (
-      'pretty_version' => '4.4.x-dev',
-      'version' => '4.4.9999999.9999999-dev',
+      'pretty_version' => '4.5.x-dev',
+      'version' => '4.5.9999999.9999999-dev',
       'aliases' => 
       array (
       ),
-      'reference' => '04b62bb8945e4b2983cfc8d1230addc555371136',
+      'reference' => 'c5a0e845031b44c1d5f1a3bb29e943c2d836d2b9',
     ),
   ),
 );
